055602637e87680fb396caeb12efdeb8fe9e74b0
barvo <barvo@cisco.com>
2023-03-02 11:24:21 -0600

    chore: add code comment for .mjs icon map file

--- framework/components/AIcon/atomicMap.mjs
+++ framework/components/AIcon/atomicMap.mjs
@@ -1,3 +1,9 @@
+/**
+ * This file is explicitly saved as an ECMAScript Module
+ * (.mjs) so that other .mjs scripts in this repo can
+ * import it properly.
+ */
+
 export const iconNameMap = {
   add: "plus",
   "add-to-list": "file-plus",
