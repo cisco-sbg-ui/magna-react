a8976213e539f542a9c0151e71223029e4e0e7c3
Robert Harris <rwharrisjr@users.noreply.github.com>
2023-03-20 16:16:21 -0500

    Add ASpinLoader component

--- framework/components/ALoader/index.js
+++ framework/components/ALoader/index.js
@@ -1,5 +1,6 @@
 import ACiscoLoader from "./ACiscoLoader";
 import ADotLoader from "./ADotLoader";
 import APageLoader from "./APageLoader";
+import ASpinLoader from "./ASpinLoader";
 
-export {ACiscoLoader, ADotLoader, APageLoader};
+export {ACiscoLoader, ADotLoader, APageLoader, ASpinLoader};
