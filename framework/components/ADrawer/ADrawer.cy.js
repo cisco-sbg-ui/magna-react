b9f182b115d4fabfed4ab75c9201606278a86dbb
barvo <barvo@cisco.com>
2023-03-24 14:54:40 -0500

    test(ADrawer): add tests for drawer triggered by AMenu

--- framework/components/ADrawer/ADrawer.cy.js
+++ framework/components/ADrawer/ADrawer.cy.js
@@ -1,7 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import AButton from "../AButton";
 import ADrawer from "./ADrawer";
 import ADrawerContent from "./ADrawerContent";
+import AListItem from "../AList/AListItem";
+import AMenu from "../AMenu/AMenu";
+import usePopupQuickExit from "../../hooks/usePopupQuickExit/usePopupQuickExit";
 
 const openDrawer = () => {
   cy.getByDataTestId("open-btn").click();
@@ -103,6 +106,67 @@ describe("<ADrawer />", () => {
       cy.getByDataTestId("drawer").invoke("outerWidth").should("eq", 800);
     });
   });
+
+  describe("when integrating with usePopupQuickExit", () => {
+    beforeEach(() => {
+      cy.mount(<DrawerTriggeredByMenuTest />);
+    });
+
+    it("should close the drawer on an escape keydown event", () => {
+      cy.getByDataTestId("open-menu-btn")
+        .click()
+        .then(() => {
+          return cy.getByDataTestId("open-drawer-btn").click();
+        })
+        .then(() => {
+          cy.getByDataTestId("drawer-content-btn").should("exist");
+          return cy.escapeKeydown();
+        })
+        .then(() => {
+          /**
+           * @todo this should become "not.exist" if PR #319 is merged
+           * @see https://github.com/cisco-sbg-ui/magna-react/pull/319
+           */
+          cy.getByDataTestId("drawer-content-btn").should("not.be.visible");
+        });
+    });
+
+    it("should close the drawer on an outside click", () => {
+      cy.getByDataTestId("open-menu-btn")
+        .click()
+        .then(() => {
+          return cy.getByDataTestId("open-drawer-btn").click();
+        })
+        .then(() => {
+          cy.getByDataTestId("drawer-content-btn").should("exist");
+          return cy.get("body").click();
+        })
+        .then(() => {
+          /**
+           * @todo this should become "not.exist" if PR #319 is merged
+           * @see https://github.com/cisco-sbg-ui/magna-react/pull/319
+           */
+          cy.getByDataTestId("drawer-content-btn").should("not.be.visible");
+        });
+    });
+
+    /**
+     * @see https://github.com/cisco-sbg-ui/magna-react/issues/143#issuecomment-1412172046
+     */
+    it("should not close the drawer when clicking a button inside the drawer", () => {
+      cy.getByDataTestId("open-menu-btn")
+        .click()
+        .then(() => {
+          return cy.getByDataTestId("open-drawer-btn").click();
+        })
+        .then(() => {
+          return cy.getByDataTestId("drawer-content-btn").click();
+        })
+        .then(() => {
+          cy.getByDataTestId("drawer-content-btn").should("be.visible");
+        });
+    });
+  });
 });
 
 function DrawerTest({asModal = true, slideIn = "right", openWidth}) {
@@ -124,6 +188,60 @@ function DrawerTest({asModal = true, slideIn = "right", openWidth}) {
         <ADrawerContent data-testid="drawer-content">
           <h3 data-testid="drawer-content">drawer content</h3>
         </ADrawerContent>
+      </ADrawer>
+    </>
+  );
+}
+
+/**
+ *
+ * @see https://github.com/cisco-sbg-ui/magna-react/issues/143
+ */
+function DrawerTriggeredByMenuTest() {
+  const triggerRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const popupRef = useRef();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  usePopupQuickExit({
+    popupRef,
+    isEnabled: isModalOpen,
+    onExit: () => setIsModalOpen(false)
+  });
+
+  return (
+    <>
+      <AButton
+        data-testid="open-menu-btn"
+        ref={triggerRef}
+        onClick={() => setIsMenuOpen(true)}
+      >
+        open menu
+      </AButton>
+      <AMenu
+        anchorRef={triggerRef}
+        open={isMenuOpen}
+        placement="bottom-left"
+        onClose={() => setIsMenuOpen(false)}
+        style={{borderRadius: 0}}
+      >
+        <AListItem
+          data-testid="open-drawer-btn"
+          onClick={() => setIsModalOpen(true)}
+        >
+          open drawer
+        </AListItem>
+      </AMenu>
+      <ADrawer
+        ref={popupRef}
+        asModal={false}
+        isOpen={isModalOpen}
+        slideIn="right"
+      >
+        drawer content
+        <AButton data-testid="drawer-content-btn">
+          drawer content button
+        </AButton>
       </ADrawer>
     </>
   );
