5772d269dac9f062e291384e95374c3ebcf1cb62
barvo <barvo@cisco.com>
2023-04-04 14:40:08 -0500

    docs(AMount): add example for wrapper element

--- docs/mock_modules.js
+++ docs/mock_modules.js
@@ -1,17 +1,18 @@
+import {useEffect, useState} from "react";
 /**
  * These are modules that can be used inside of documentation
  * to provide more enriching examples. The purpose of keeping
  * these functions in this file is to abstract away unnecessary
  * implementation details for setting up an example in an effort
  * to focus more on the component's usage itself.
- * 
+ *
  * @example using the randomColors in documentation
  * const randomColors = mockImport('randomColors');
  * randomColors.get().then(randomColors => // use random colors);
  */
 const modules = {
-  randomColors: (function() {
-    const getHex = (num) => num.toString(16).padStart(2, '0');
+  randomColors: (function () {
+    const getHex = (num) => num.toString(16).padStart(2, "0");
     const getRandomRgb = () => {
       const r = Math.floor(Math.random() * 256);
       const g = Math.floor(Math.random() * 256);
@@ -19,8 +20,10 @@ const modules = {
       return {r, g, b};
     };
     const rgbToHex = ({r, g, b}) => {
-      const rHex = getHex(r), gHex = getHex(g), bHex = getHex(b);
-      return '#' + rHex + gHex + bHex;
+      const rHex = getHex(r),
+        gHex = getHex(g),
+        bHex = getHex(b);
+      return "#" + rHex + gHex + bHex;
     };
     const generateRandomColors = (maxColors = 25) => {
       const colors = [];
@@ -29,9 +32,9 @@ const modules = {
         const hex = rgbToHex(rgb);
         colors.push({
           ...rgb,
-          hex,
+          hex
         });
-      };
+      }
       return colors;
     };
     const get = (num = 25) => {
@@ -39,9 +42,46 @@ const modules = {
         setTimeout(() => resolve(generateRandomColors(num)), 1000);
       });
     };
-    return { get };
+    return {get};
   })(),
-}
+  useDraggable: (function () {
+    const useDraggable = (el) => {
+      const [{dx, dy}, setOffset] = useState({dx: 0, dy: 0});
+      useEffect(() => {
+        if (!el.current) return;
+        const draggableEl = el.current;
+        draggableEl.style.position = "relative";
+        const handleMouseDown = (event) => {
+          const startX = event.pageX - dx;
+          const startY = event.pageY - dy;
+          const handleMouseMove = (event) => {
+            const newDx = event.pageX - startX;
+            const newDy = event.pageY - startY;
+            setOffset({dx: newDx, dy: newDy});
+          };
+          document.addEventListener("mousemove", handleMouseMove);
+          document.addEventListener(
+            "mouseup",
+            () => {
+              document.removeEventListener("mousemove", handleMouseMove);
+            },
+            {once: true}
+          );
+        };
+        draggableEl.addEventListener("mousedown", handleMouseDown);
+        return () => {
+          if (!draggableEl) return;
+          draggableEl.removeEventListener("mousedown", handleMouseDown);
+        };
+      }, [dx, dy, el]);
+
+      useEffect(() => {
+        el.current.style.transform = `translate3d(${dx}px, ${dy}px, 0)`;
+      }, [dx, dy, el]);
+    };
+    return {useDraggable};
+  })()
+};
 
 function mockImport(moduleName) {
   const requestedModule = modules[moduleName];
