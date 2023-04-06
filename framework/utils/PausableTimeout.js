bbb987741b0ff1fae2e3aabeef1236cfd61a58e8
Robert Harris <rwharrisjr@users.noreply.github.com>
2023-04-06 11:19:16 -0500

    feat: Pause toast timeout on hover (#342)
    * feat: Pause toast timeout on hover

---
+++ framework/utils/PausableTimeout.js
@@ -0,0 +1,49 @@
+class PausableTimeout {
+  constructor(callback, delay) {
+    this.callback = callback;
+    this.delay = delay;
+    this.remaining = delay;
+
+    this.resume();
+  }
+
+  restart() {
+    this.clear();
+    this.resume();
+  }
+
+  resume() {
+    this.running = true;
+    this.started = new Date();
+    this.id = setTimeout(() => {
+      this.callback();
+    }, this.remaining);
+  }
+
+  pause() {
+    this.running = false;
+    clearTimeout(this.id);
+    this.remaining -= new Date() - this.started;
+  }
+
+  clear() {
+    this.running = false;
+    this.remaining = this.delay;
+    clearTimeout(this.id);
+  }
+
+  getTimeLeft() {
+    if (this.running) {
+      this.pause();
+      this.resume();
+    }
+
+    return this.remaining;
+  }
+
+  getStateRunning() {
+    return this.running;
+  }
+}
+
+export default PausableTimeout;
