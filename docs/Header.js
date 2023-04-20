07fffa17a2c656ef3c48a985540ef43691e6c2e5
barvo <barvo@cisco.com>
2023-04-20 10:19:22 -0500

    docs: add mobile responsiveness

--- docs/Header.js
+++ docs/Header.js
@@ -1,7 +1,8 @@
-import {AButton, AIcon, ARow, useATheme} from "../framework";
+import {AButton, AIcon, ARow, useATheme, useMediaQuery} from "../framework";
 import ThemeSwitcher from "./ThemeSwitcher";
 
-const Header = ({className}) => {
+const Header = ({className, onHamburgerClick}) => {
+  const {matches: isMobile} = useMediaQuery("(max-width: 800px)");
   const {isDark} = useATheme();
   return (
     <ARow
@@ -27,14 +28,23 @@ const Header = ({className}) => {
           zIndex: "9"
         }}
       >
-        <h1 className="ma-0" style={{fontSize: "2rem"}}>
-          <AButton className="pa-0 mr-1" tertiaryAlt icon href="/">
-            <AIcon size={24} className="mds-blue--blue-10--text">
-              circle
-            </AIcon>
-          </AButton>{" "}
-          Magna-React
-        </h1>
+        <div className="d-flex align-center">
+          {isMobile && (
+            <AButton onClick={onHamburgerClick} icon tertiaryAlt>
+              <AIcon size={24}>list</AIcon>
+            </AButton>
+          )}
+          <h1 className="ma-0" style={{fontSize: isMobile ? "1.5rem" : "2rem"}}>
+            <AButton className="pa-0 mr-1" tertiaryAlt icon href="/">
+              {!isMobile && (
+                <AIcon size={24} className="mds-blue--blue-10--text">
+                  circle
+                </AIcon>
+              )}
+            </AButton>{" "}
+            Magna-React
+          </h1>
+        </div>
         <div>
           <AButton
             href="https://github.com/cisco-sbg-ui/magna-react"
