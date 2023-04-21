127f111798a297b920e480232dc2e9ec570f1518
barvo <barvo@cisco.com>
2023-04-20 23:42:07 -0500

    docs: add beta note to header

--- docs/Header.js
+++ docs/Header.js
@@ -5,67 +5,100 @@ const Header = ({className, onHamburgerClick}) => {
   const {matches: isMobile} = useMediaQuery("(max-width: 1000px)");
   const {isDark} = useATheme();
   return (
-    <ARow
-      className={`${className} py-2 ${
-        isDark ? "mds-neutral--neutral-16" : "mds-neutral--neutral-1"
-      }`}
-      noGutters
-      style={{
-        borderBottom: isDark
-          ? "0.0625rem solid rgb(20, 21, 23)"
-          : "0.0625rem solid #e9ecef",
-        width: "100%",
-        position: "sticky",
-        top: "0",
-        zIndex: "3"
-      }}
-    >
-      <div
-        className="d-flex justify-space-between align-center"
+    <div style={{position: "sticky", top: "0", zIndex: "3"}}>
+      <ARow
+        className={`${className} py-2 d-flex white--text justify-center ${
+          isDark ? "mds-neutral--neutral-16" : "mds-neutral--neutral-1"
+        }`}
+        noGutters
         style={{
-          height: "50px",
+          //backgroundImage: "linear-gradient(to right, #373c42, #a7adb5)",
+          backgroundImage: "linear-gradient(to right, #306acf, #6c9ef5)",
+          borderBottom: isDark
+            ? "0.0625rem solid rgb(20, 21, 23)"
+            : "0.0625rem solid #e9ecef",
           width: "100%",
-          zIndex: "9"
+          position: "sticky",
+          top: "0",
+          zIndex: "3"
         }}
       >
-        <div className="d-flex align-center">
-          {isMobile && (
-            <AButton onClick={onHamburgerClick} icon tertiaryAlt>
-              <AIcon size={24}>list</AIcon>
-            </AButton>
-          )}
-          <h1 className="ma-0" style={{fontSize: isMobile ? "1.2rem" : "2rem"}}>
-            <AButton className="pa-0 mr-1" tertiaryAlt icon href="/">
-              {!isMobile && (
+        <div>
+          These docs are in beta. Please report issues to{" "}
+          <a
+            className="white--text"
+            href="https://github.com/cisco-sbg-ui/magna-react/issues/new/choose"
+          >
+            GitHub
+          </a>
+          .
+        </div>
+      </ARow>
+      <ARow
+        className={`${className} py-2 ${
+          isDark ? "mds-neutral--neutral-16" : "mds-neutral--neutral-1"
+        }`}
+        noGutters
+        style={{
+          borderBottom: isDark
+            ? "0.0625rem solid rgb(20, 21, 23)"
+            : "0.0625rem solid #e9ecef",
+          width: "100%"
+          // position: "sticky",
+          // top: "0",
+          // zIndex: "3"
+        }}
+      >
+        <div
+          className="d-flex justify-space-between align-center"
+          style={{
+            height: "50px",
+            width: "100%",
+            zIndex: "9"
+          }}
+        >
+          <div className="d-flex align-center">
+            {isMobile && (
+              <AButton onClick={onHamburgerClick} icon tertiaryAlt>
+                <AIcon size={24}>list</AIcon>
+              </AButton>
+            )}
+            <h1
+              className="ma-0"
+              style={{fontSize: isMobile ? "1.2rem" : "2rem"}}
+            >
+              <AButton className="pa-0 mr-1" tertiaryAlt icon href="/">
+                {/* {!isMobile && (
                 <AIcon size={24} className="mds-blue--blue-10--text">
                   circle
                 </AIcon>
-              )}
-            </AButton>{" "}
-            Magna-React
-          </h1>
-        </div>
-        <div>
-          <AButton
-            href="https://github.com/cisco-sbg-ui/magna-react"
-            icon
-            tertiary
-          >
-            <AIcon
-              className={
-                isDark
-                  ? "mds-neutral--neutral-5--text"
-                  : "mds-neutral--neutral-14--text"
-              }
-              size={24}
+              )} */}
+              </AButton>{" "}
+              Magna-React
+            </h1>
+          </div>
+          <div>
+            <AButton
+              href="https://github.com/cisco-sbg-ui/magna-react"
+              icon
+              tertiary
             >
-              code
-            </AIcon>
-          </AButton>
-          <ThemeSwitcher style={{alignSelf: "flex-end"}} />
+              <AIcon
+                className={
+                  isDark
+                    ? "mds-neutral--neutral-5--text"
+                    : "mds-neutral--neutral-14--text"
+                }
+                size={24}
+              >
+                code
+              </AIcon>
+            </AButton>
+            <ThemeSwitcher style={{alignSelf: "flex-end"}} />
+          </div>
         </div>
-      </div>
-    </ARow>
+      </ARow>
+    </div>
   );
 };
 
