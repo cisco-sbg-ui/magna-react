2dce44148e5c01c69972e8f69b573ac0a5b500c9
Brennan Arvo <94568316+brennarvo@users.noreply.github.com>
2023-08-09 10:06:40 -0500

    fix(useFocusTrap): preserve sequential tab order when initially focused element is overridden (#483)
    * Resolves #482 
    * Resolves #486

--- framework/hooks/useFocusTrap/useFocusTrap.cy.js
+++ framework/hooks/useFocusTrap/useFocusTrap.cy.js
@@ -43,6 +43,56 @@ describe("useFocusTrap()", () => {
 
     cy.get("textarea").should("have.focus");
   });
+
+  it("should preserve tab order when an element within the focus trap is explicitly clicked", () => {
+    cy.mount(<FocusTrapTest />);
+
+    cy.get("#trap").should("have.focus");
+
+    cy.getByDataTestId("trapped-item-2").click();
+    cy.getByDataTestId("trapped-item-2").should("have.focus");
+
+    cy.get("body").tab();
+    cy.getByDataTestId("trapped-item-3").should("have.focus");
+
+    cy.get("body").tab();
+    cy.getByDataTestId("trapped-item-1").should("have.focus");
+
+    cy.getByDataTestId("trapped-item-3").click();
+    cy.get("body").tab({shift: true});
+    cy.getByDataTestId("trapped-item-2").should("have.focus");
+  });
+
+  it("should preserve tab order when the inner element receives auto focus", () => {
+    cy.mount(<AutoInitialFocusTest />);
+
+    cy.get("textarea").should("have.focus");
+    cy.get("body").tab();
+    cy.getByDataTestId("trapped-item-3").should("have.focus");
+  });
+
+  it("should preserve tab order when another element is passed to set initial focus to", () => {
+    cy.mount(<CustomInitialFocusTest />);
+
+    cy.getByDataTestId("trapped-item-2").should("have.focus");
+    cy.get("body").tab();
+    cy.getByDataTestId("trapped-item-3").should("have.focus");
+  });
+
+  it("should reset the tab order when a non-focusable element within the focus trap is explicitly clicked", () => {
+    cy.mount(<FocusTrapTest />);
+
+    cy.get("#trap").should("have.focus");
+
+    cy.getByDataTestId("trapped-item-2").click();
+    cy.getByDataTestId("trapped-item-2").should("have.focus");
+
+    cy.get("label").eq(1).click();
+    cy.getByDataTestId("trapped-item-2").should("not.have.focus");
+
+    cy.get("body").tab();
+    cy.getByDataTestId("trapped-item-1").should("have.focus");
+  });
 });
 
 const FocusTrapTest = ({children, enableAutofocus = true}) => {
@@ -74,10 +124,117 @@ const FocusTrapTest = ({children, enableAutofocus = true}) => {
           <label htmlFor="password">Password</label>
           <input data-testid="trapped-item-2" type="password" />
           <br />
-          <button data-testid="trapped-item-3">Submit</button>
+          <button data-testid="trapped-item-3" type="button">
+            Submit
+          </button>
         </form>
 
         {children}
+      </div>
+
+      <div>
+        {[...new Array(3).keys()]
+          .map((num) => num + 3)
+          .map((num) => (
+            <a
+              key={num}
+              data-testid={`inaccessible-item-${num}`}
+              href={`#link-${num}`}
+            >
+              link {num}
+            </a>
+          ))}
+      </div>
+    </>
+  );
+};
+
+const CustomInitialFocusTest = () => {
+  const trapContainerRef = useRef();
+  const initialFocusElRef = useRef();
+  useFocusTrap({
+    rootRef: trapContainerRef,
+    isEnabled: true,
+    autoFocusElementRef: initialFocusElRef
+  });
+  return (
+    <>
+      <div>
+        {[...new Array(3).keys()].map((num) => (
+          <a
+            key={num}
+            data-testid={`inaccessible-item-${num}`}
+            href={`#link-${num}`}
+          >
+            link {num}
+          </a>
+        ))}
+      </div>
+
+      <div id="trap" tabIndex={-1} ref={trapContainerRef}>
+        <form>
+          <label htmlFor="username">Username</label>
+          <input data-testid="trapped-item-1" id="username" type="text" />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input
+            ref={initialFocusElRef}
+            data-testid="trapped-item-2"
+            type="password"
+          />
+          <br />
+          <button data-testid="trapped-item-3">Submit</button>
+        </form>
+      </div>
+
+      <div>
+        {[...new Array(3).keys()]
+          .map((num) => num + 3)
+          .map((num) => (
+            <a
+              key={num}
+              data-testid={`inaccessible-item-${num}`}
+              href={`#link-${num}`}
+            >
+              link {num}
+            </a>
+          ))}
+      </div>
+    </>
+  );
+};
+
+const AutoInitialFocusTest = () => {
+  const trapContainerRef = useRef();
+  useFocusTrap({
+    rootRef: trapContainerRef,
+    isEnabled: true,
+    autoFocusElementRef: null
+  });
+  return (
+    <>
+      <div>
+        {[...new Array(3).keys()].map((num) => (
+          <a
+            key={num}
+            data-testid={`inaccessible-item-${num}`}
+            href={`#link-${num}`}
+          >
+            link {num}
+          </a>
+        ))}
+      </div>
+
+      <div id="trap" tabIndex={-1} ref={trapContainerRef}>
+        <form>
+          <label htmlFor="username">Username</label>
+          <input data-testid="trapped-item-1" id="username" type="text" />
+          <br />
+          <label htmlFor="password">Password</label>
+          <ATextarea autoFocus />
+          <br />
+          <button data-testid="trapped-item-3">Submit</button>
+        </form>
       </div>
 
       <div>
--- framework/hooks/useFocusTrap/useFocusTrap.js
+++ framework/hooks/useFocusTrap/useFocusTrap.js
@@ -1,5 +1,9 @@
-import {useEffect} from "react";
-import {isBackwardTab, isForwardTab} from "../../utils/helpers";
+import {useCallback, useEffect} from "react";
+import {
+  canNodeReceiveFocus,
+  isBackwardTab,
+  isForwardTab
+} from "../../utils/helpers";
 
 function createTrap(walker) {
   return function trap(e) {
@@ -37,35 +41,87 @@ export default function useFocusTrap({
   autoFocusElementRef,
   isEnabled = true
 }) {
+  const getTreeWalkerNodeDisposition = useCallback(
+    (node) => {
+      if (node === rootRef?.current || canNodeReceiveFocus(node)) {
+        return NodeFilter.FILTER_ACCEPT;
+      } else {
+        return NodeFilter.FILTER_SKIP;
+      }
+    },
+    [rootRef]
+  );
+
   useEffect(() => {
+    let trapContainerEl;
+
     if (isEnabled && rootRef.current) {
+      trapContainerEl = rootRef.current;
+
       const treeWalker = document.createTreeWalker(
-        rootRef.current,
+        trapContainerEl,
         NodeFilter.SHOW_ELEMENT,
         {
-          acceptNode: (node) => {
-            if (node === rootRef.current) {
-              return NodeFilter.FILTER_ACCEPT;
-            }
-            return node.tabIndex < 0
-              ? NodeFilter.FILTER_SKIP
-              : NodeFilter.FILTER_ACCEPT;
-          }
+          acceptNode: getTreeWalkerNodeDisposition
         }
       );
+
       if (autoFocusElementRef) {
-        const animationPromises = rootRef.current
+        const animationPromises = trapContainerEl
           .getAnimations({subtree: true})
           .map((animation) => animation.finished);
-        Promise.allSettled(animationPromises).then(() =>
-          (autoFocusElementRef.current ?? rootRef.current).focus({
+        Promise.allSettled(animationPromises).then(() => {
+          const initialFocusNode =
+            autoFocusElementRef.current ?? trapContainerEl;
+
+          initialFocusNode.focus({
             focusVisible: true
-          })
-        );
+          });
+
+          // Point the `TreeWalker` instance's active node to the one the developer
+          // passed in order to preserver tab order within the trap.
+          treeWalker.currentNode = initialFocusNode;
+        });
+      } else if (
+        trapContainerEl.contains(document.activeElement) &&
+        getTreeWalkerNodeDisposition(document.activeElement) ===
+          NodeFilter.FILTER_ACCEPT
+      ) {
+        // This covers the situation when a developer passes `autoFocusElement` as a
+        // falsy value to prevent `useFocusTrap` from stealing initial focus from any
+        // externally defined focus logic, e.g., an `autofocus` attribute on an element.
+        // Point the `TreeWalker` instance's active node to one that was externally
+        // focused to within the trap, e.g., one with an `autofocus` attribute.
+        treeWalker.currentNode = document.activeElement;
       }
+
       const trap = createTrap(treeWalker);
       document.addEventListener("keydown", trap);
-      return () => document.removeEventListener("keydown", trap);
+
+      function resetTreeWalkerHeadOnClick(e) {
+        const target = e.target;
+
+        if (canNodeReceiveFocus(target)) {
+          // The user is clicking a focusable element within the trap, so
+          // we need to ensure that their next `Tab` or `Shift` + `Tab`
+          // keydown focuses to an element closest to the one just clicked.
+          treeWalker.currentNode = target;
+        } else {
+          // The user is clicking a non-focusable element within the trap, so
+          // we should reset the tab order sequence to start from the beginning.
+          treeWalker.currentNode = trapContainerEl;
+        }
+      }
+
+      trapContainerEl.addEventListener("mousedown", resetTreeWalkerHeadOnClick);
+
+      return () => {
+        document.removeEventListener("keydown", trap);
+        trapContainerEl.removeEventListener(
+          "mousedown",
+          resetTreeWalkerHeadOnClick
+        );
+      };
     }
-  }, [rootRef, autoFocusElementRef, isEnabled]);
+  }, [rootRef, autoFocusElementRef, isEnabled, getTreeWalkerNodeDisposition]);
 }
