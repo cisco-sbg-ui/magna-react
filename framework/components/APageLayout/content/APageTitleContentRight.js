1ecc5425cbef0e1228216541b4c5134bd7c80251
Robert Harris <rwharrisjr@users.noreply.github.com>
2023-09-14 12:28:27 -0500

    Add option for no left content for right aligned header content (#520)

--- framework/components/APageLayout/content/APageTitleContentRight.js
+++ framework/components/APageLayout/content/APageTitleContentRight.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const APageTitleContentRight = ({
   className: propsClassName,
   children,
+  noLeftContent,
   ...rest
 }) => {
   let className = "a-page-title-content__right";
@@ -11,11 +13,20 @@ const APageTitleContentRight = ({
     className += ` ${propsClassName}`;
   }
 
+  if (noLeftContent) {
+    className += " a-page-title-content__right--noLeftContent";
+  }
+
   return (
     <div className={className} {...rest}>
       {children}
     </div>
   );
+};
+
+APageTitleContentRight.propTypes = {
+  /** Set to true if using for right aligned content without APageTitleContentLeft */
+  noLeftContent: PropTypes.bool
 };
 
 APageTitleContentRight.displayName = "APageTitleContentRight";
