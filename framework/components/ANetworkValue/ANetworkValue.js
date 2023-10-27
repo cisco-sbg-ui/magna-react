b338c65a847e7a2960817ca6d99d0d715b7ed4db
Robert Harris <rwharrisjr@users.noreply.github.com>
2023-10-27 11:15:50 -0500

    Use RobotoMono font (#558)

--- framework/components/ANetworkValue/ANetworkValue.js
+++ framework/components/ANetworkValue/ANetworkValue.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ACopyButton from "../ACopyButton";
 import "./ANetworkValue.scss";
 
 const baseClass = "a-network-value";
 
-const ANetworkValue = ({className: propsClassName, children, ...rest}) => {
+const ANetworkValue = ({
+  className: propsClassName,
+  children,
+  copyButton,
+  copyValue,
+  ...rest
+}) => {
   let className = baseClass;
 
   if (propsClassName) {
@@ -13,12 +20,30 @@ const ANetworkValue = ({className: propsClassName, children, ...rest}) => {
 
   return (
     <div className={className} {...rest}>
-      {children}
+      <div className={`${baseClass}__value`}>{children}</div>
+      {copyButton && (
+        <div className={`${baseClass}__copy`}>
+          <ACopyButton
+            value={copyValue || children}
+            defaultLabel={false}
+            small
+          />
+        </div>
+      )}
     </div>
   );
 };
 
-ANetworkValue.propTypes = {};
+ANetworkValue.propTypes = {
+  /**
+   * Show a copy button next to the vaulue
+   */
+  copyButton: PropTypes.bool,
+  /**
+   * Value to copy, if different from `children`
+   */
+  copyValue: PropTypes.string
+};
 
 ANetworkValue.displayName = "ANetworkValue";
 
