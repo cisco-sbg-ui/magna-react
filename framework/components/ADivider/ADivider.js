56e5607e336210a71e2c81d34cd2ed746cc36e78
Andrea <androche@cisco.com>
2023-11-17 15:08:26 -0500

    feat: divider, menu, pagniation, app bg

--- framework/components/ADivider/ADivider.js
+++ framework/components/ADivider/ADivider.js
@@ -4,15 +4,10 @@ import React, {forwardRef} from "react";
 import "./ADivider.scss";
 
 const ADivider = forwardRef(
-  (
-    {className: propsClassName, light, lighter, role = "separator", ...rest},
-    ref
-  ) => {
+  ({className: propsClassName, light, role = "separator", ...rest}, ref) => {
     let className = "a-divider";
 
-    if (lighter) {
-      className += " a-divider--color-lighter";
-    } else if (light) {
+    if (light) {
       className += " a-divider--color-light";
     }
 
@@ -33,10 +28,6 @@ ADivider.propTypes = {
    * Toggles the light variant.
    */
   light: PropTypes.bool,
-  /**
-   * Toggles the lighter variant.
-   */
-  lighter: PropTypes.bool,
   /**
    * Sets the [WAI-ARIA](https://www.w3.org/WAI/standards-guidelines/aria/) role.
    */
