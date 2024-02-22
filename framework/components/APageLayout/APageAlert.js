f06b180dc18402266fa432fc3f153d0f1b7ce40a
Brennan Arvo <barvo@cisco.com>
2024-02-22 16:56:13 -0600

    feat: add "APageAlert" component

---
+++ framework/components/APageLayout/APageAlert.js
@@ -0,0 +1,24 @@
+import React, {forwardRef} from "react";
+import "./APageAlert.scss";
+import AToast from "../AToast";
+
+const APageAlert = forwardRef(({className: propsClassName, ...rest}, ref) => {
+  let className = "a-page-alert";
+
+  if (propsClassName) {
+    className += ` ${propsClassName}`;
+  }
+
+  return <AToast {...rest} className={className} ref={ref} />;
+});
+
+APageAlert.propTypes = {
+  /**
+   * String representing class names to be passed `AToast` component.
+   */
+  className: PropTypes.string
+};
+
+APageAlert.displayName = "APageAlert";
+
+export default APageAlert;
