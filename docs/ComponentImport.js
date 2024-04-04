2adbd98a1b1006399658e8ed6926d43fb9f5a435
Robert Harris <23561587+rwharrisjr@users.noreply.github.com>
2024-04-04 15:36:05 -0500

    remove @

--- docs/ComponentImport.js
+++ docs/ComponentImport.js
@@ -9,7 +9,7 @@ const ComponentImport = ({children, components}) => {
   const startBracket = isLarge ? "{\n  " : "{ ";
   const endBracket = isLarge ? "\n}" : " }";
 
-  let importStatement = `import ${startBracket}${componentImports}${endBracket} from '@cisco-sbg-ui/@magna-react'`;
+  let importStatement = `import ${startBracket}${componentImports}${endBracket} from '@cisco-sbg-ui/magna-react'`;
 
   return <CodeBlock code={importStatement} />;
 };
