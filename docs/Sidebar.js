1c5196ce7b92d30b8c7b1353878cad36934c51b8
Robert Harris <23561587+rwharrisjr@users.noreply.github.com>
2024-04-09 09:01:57 -0500

    adjust padding

--- docs/Sidebar.js
+++ docs/Sidebar.js
@@ -181,7 +181,7 @@ const Sidebar = ({
       position="relative"
       isOpen={true}
       id="sidebar"
-      className="root-sidebar py-4 px-5 sidebar"
+      className="root-sidebar py-4 px-1 sidebar"
       openWidth={"288px"}
       style={{
         height: "100%",
