a96223f254aba03bb804c00cfff42879985e2324
Brennan Arvo <barvo@cisco.com>
2024-04-29 16:02:28 -0500

    test: add test id to neutral icon

--- framework/components/AActivityTimeline/icons/NeutralIcon.js
+++ framework/components/AActivityTimeline/icons/NeutralIcon.js
@@ -11,6 +11,7 @@ function NeutralIcon() {
       height="18"
       viewBox="0 0 18 18"
       width="18"
+      data-testid="AActivityTimelineItemIcon-Neutral"
       className="a-activity-timeline__item__status-icon"
       xmlns="http://www.w3.org/2000/svg"
     >
