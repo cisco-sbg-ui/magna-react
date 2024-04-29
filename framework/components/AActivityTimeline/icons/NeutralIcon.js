ba3f844673a6219ba65d580195fc3e0a0bd733b5
Brennan Arvo <barvo@cisco.com>
2024-04-29 15:48:05 -0500

    refactor: use consistent icon svgs from magnetic site

---
+++ framework/components/AActivityTimeline/icons/NeutralIcon.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../AActivityTimeline.scss";
+
+/**
+ * Copied from https://magnetic-react.cisco.com/?path=/docs/components-activity-timeline--status#status-timeline
+ */
+function NeutralIcon() {
+  return (
+    <svg
+      fill="none"
+      height="18"
+      viewBox="0 0 18 18"
+      width="18"
+      className="a-activity-timeline__item__status-icon"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="9"
+        cy="9"
+        fill="var(--interact-bg-default)"
+        r="8"
+        stroke="var(--interact-border-weak-default)"
+        stroke-width="2"
+      ></circle>
+    </svg>
+  );
+}
+
+export default NeutralIcon;
