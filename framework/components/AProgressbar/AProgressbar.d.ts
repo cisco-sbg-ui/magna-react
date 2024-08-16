d8dea1fb2ff782af14de271d2994ab0898dcc681
Andrea <androche@cisco.com>
2024-08-16 11:37:44 -0400

    feat: swap to typescript and fix up animation

--- framework/components/AProgressbar/AProgressbar.d.ts
+++
@@ -1,5 +0,0 @@
-import {type AProgressbarProps} from "./types";
-
-declare const AProgressbar: (props: AProgressbarProps) => JSX.Element;
-
-export default AProgressbar;
--- framework/components/AProgressbar/types.ts
+++ framework/components/AProgressbar/types.ts
@@ -1,41 +1,56 @@
 import {Override} from "../../types";
 
-export type AProgressbarSize = "small" | "medium" | "large";
+export type AProgressbarSize = "small" | "medium";
+
+export type AProgressbarStatus = "active" | "success" | "error";
 
 export type AProgressbarProps = Override<
   React.ComponentPropsWithRef<"div">,
   {
     /**
      * Determines the duration of the progress bar (if animated).
-     * @example 0.3s
      */
     animationDuration?: string;
     /**
      * Sets the size of the indicator.
+     *  @deprecated use small or medium convention instead
      */
     size?: AProgressbarSize;
+    /**
+     * Toggles the `disabled` state.
+     * @deprecated no longer used
+     */
+    disabled?: boolean;
     /**
      * Toggles whether the percentage is displayed additionally as text.
      */
     displayText?: boolean;
     /**
      * The percent completed.
-     *
-     * @defaultValue `0`
      */
     percentage?: number;
     /**
      * Toggles the striped display variant.
+     *  @deprecated use animate instead
      */
     striped?: boolean;
     /**
+     * Toggles the animation display.
+     */
+    animate?: boolean;
+    /**
      * Adds animation for an indeterminate progress
+     *  @deprecated no longer used
      */
     indeterminate?: boolean;
     /**
      * Class to apply to the bar element
      */
     barClassName?: string;
+    /**
+     * Class to apply to the fill element
+     */
+    fillClassName?: string;
     /**
      * Style to apply to the bar element
      */
@@ -44,5 +59,36 @@ export type AProgressbarProps = Override<
      * Style to apply to the fill element
      */
     fillStyle?: React.CSSProperties;
+    /**
+     * The status of the progress bar.
+     * @defaultValue `"active"`
+     * */
+    status?: AProgressbarStatus;
+    /**
+     * Subtext displayed below bar
+     */
+    helperText?: string;
+    /**
+     * Label for the progress bar
+     */
+    label?: string;
+    /**
+     * Label and helper text block positioning
+     */
+    stacked?: boolean;
+    /**
+     * Centers the label and helper text block
+     */
+    centered?: boolean;
+    /**
+     * The status of the progress bar.
+     * @defaultValue `"medium"`
+     * */
+    small?: boolean;
+    /**
+     * The status of the progress bar.
+     * @defaultValue `"medium"`
+     * */
+    medium?: boolean;
   }
 >;
