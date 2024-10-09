c99b3386d4294273ff8761135c79f7204f255a62
Andrea <androche@cisco.com>
2024-10-09 11:11:07 -0400

    build: eslint9

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooksRules from "eslint-plugin-react-hooks";
+import pluginCypress from "eslint-plugin-cypress/flat";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  pluginCypress.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    ignores: ["lib/*", ".next/*"]
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    settings: {
+      react: {
+        version: "18.2.0"
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        }
+      }
+    },
+    plugins: {
+      "react-hooks": reactHooksRules,
+      cypress: pluginCypress,
+      "jsx-a11y": jsxA11y
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "warn",
+      "cypress/no-unnecessary-waiting": "error",
+      "cypress/unsafe-to-chain-command": "off",
+      ...jsxA11y.configs.recommended.rules
+    }
+  }
+];
