2a67b3bf99aa9b44a6c17bd22935fcfffe7298f7
Andrea <112431822+androche@users.noreply.github.com>
2024-10-09 11:51:21 -0400

    build: eslint9 (#916)

--- framework/components/ADataTable/ADataTableCell.js
+++ framework/components/ADataTable/ADataTableCell.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 const ADataTableCell = ({className, ...rest}) => (
   <td
+    // eslint-disable-next-line jsx-a11y/no-interactive-element-to-noninteractive-role
     role="cell"
     className={`a-data-table__cell${className ? ` ${className}` : ""}`}
     {...rest}
