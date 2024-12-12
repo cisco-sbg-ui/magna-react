f891578558269034ec55420348809d79648679a3
Andrea <androche@cisco.com>
2024-12-12 13:17:58 -0500

    chore: delete atimeline

--- framework/components/ATimeline/ATimelineItemTitle.d.ts
+++
@@ -1,7 +0,0 @@
-import {type ATimelineItemTitleProps} from "./types";
-
-declare const ATimelineItemTitle: (
-  props: ATimelineItemTitleProps
-) => JSX.Element;
-
-export default ATimelineItemTitle;
