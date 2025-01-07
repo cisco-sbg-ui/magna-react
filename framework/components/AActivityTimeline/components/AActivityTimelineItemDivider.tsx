3ae527e558ed3734f189d2f87b994b40252e858f
Andrea <androche@cisco.com>
2025-01-07 14:46:02 -0500

    chore: bypass doc parser

--- framework/components/AActivityTimeline/components/AActivityTimelineItemDivider.tsx
+++ framework/components/AActivityTimeline/components/AActivityTimelineItemDivider.tsx
@@ -1,8 +1,11 @@
+import type {PropsWithChildren} from "react";
 import React from "react";
 
 import "../AActivityTimeline.scss";
 
-function AActivityTimelineItemDivider() {
+//TODO Make doc parser happy in meantime since it doesn't support empty components
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function AActivityTimelineItemDivider(props: PropsWithChildren) {
   return <hr className="a-activity-timeline__list-item__divider" />;
 }
 
