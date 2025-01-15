028454fee5e1c3984d1cc868425351d03392cf2b
Robert Harris <23561587+rwharrisjr@users.noreply.github.com>
2025-01-15 09:30:44 -0600

    fix: export TS types for ASkeleton

--- framework/components/ASkeleton/index.ts
+++ framework/components/ASkeleton/index.ts
@@ -3,4 +3,18 @@ import ASkeletonHeader from "./ASkeletonHeader";
 import ASkeletonBlock from "./ASkeletonBlock";
 import ASkeletonText from "./ASkeletonText";
 
+import type {
+  ASkeletonProps,
+  ASkeletonHeaderProps,
+  ASkeletonBlockProps,
+  ASkeletonTextProps
+} from "./types";
+
+export type {
+  ASkeletonProps,
+  ASkeletonHeaderProps,
+  ASkeletonBlockProps,
+  ASkeletonTextProps
+};
+
 export {ASkeleton, ASkeletonHeader, ASkeletonBlock, ASkeletonText};
